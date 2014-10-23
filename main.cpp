14a50c48c9d8a227e1e6b1c0819edd3ae0ec3057
Ragnis Armus <ragnis.armus@gmail.com>
2014-10-23 22:10:47 +0300

    Properly handle SIGINT

--- main.cpp
+++ main.cpp
@@ -1,17 +1,29 @@
+#include <signal.h>
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 #include "Frame.h"
+#include "comm/DebugLink.h"
 #include "objects/BallObject.h"
 #include "vp/VideoProcessor.h"
 
+bool sigint = false;
+
+void handleSigint(int signal)
+{
+	sigint = true;
+}
+
 int main(int argc, char** argv)
 {
+	signal(SIGINT, handleSigint);
+
 	cv::VideoCapture capture(0);
 
 	if (!capture.isOpened())
 	{
-		// Failed to access the camera
-		return -1;
+		DebugLink::instance().msg(DebugLink::LEVEL_ERROR, "main: Failed to initialize the camera");
+		capture.release();
+		return 1;
 	}
 
 	capture.set(CV_CAP_PROP_FRAME_WIDTH, 1280);
@@ -22,7 +34,7 @@ int main(int argc, char** argv)
 	cv::Point fpsPosition(5, 20);
 	char fpsString[16];
 
-	while (true)
+	while (!sigint)
 	{
 		Frame *frame = vp.processFrame();
 		cv::Mat preview(*frame->sourceMat);
@@ -55,6 +67,12 @@ int main(int argc, char** argv)
 
 		delete frame;
 	}
+
+	DebugLink::instance().msg(DebugLink::LEVEL_INFO, "main: Shutting down");
+	DebugLink::instance().close();
+
+	cv::destroyAllWindows();
+	cv::waitKey(0);
 
 	capture.release();
 
