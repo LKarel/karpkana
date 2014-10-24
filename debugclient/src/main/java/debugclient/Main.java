c60fe665ee65cb025f435eb98ebb648f5854f91d
Ragnis Armus <ragnis.armus@gmail.com>
2014-10-24 21:51:15 +0300

    Set frame title to DebugClient

--- debugclient/src/main/java/debugclient/Main.java
+++ debugclient/src/main/java/debugclient/Main.java
@@ -29,7 +29,7 @@ public class Main
 
 	private static void showGUI()
 	{
-		JFrame frame = new JFrame("Main");
+		JFrame frame = new JFrame("DebugClient");
 		frame.setSize(800, 600);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
