171e02c803063f3daee70afaa781c6e0b9340178
Ragnis Armus <ragnis.armus@gmail.com>
2014-10-25 17:22:52 +0300

    Remove local GUI

--- main.cpp
+++ main.cpp
@@ -39,48 +39,16 @@ int main(int argc, char** argv)
 	{
 		Frame *frame = vp.processFrame();
 
-		if (!frame)
+		if (frame)
 		{
-			continue;
+			DebugLink::instance().frame(frame);
+			delete frame;
 		}
-
-		cv::Mat preview(*frame->sourceMat);
-
-		for (std::vector<BaseObject *>::size_type i = 0; i != frame->objects.size(); ++i)
-		{
-			switch (frame->objects[i]->type)
-			{
-				case BallObject::TYPE:
-				{
-					BallObject *ball = (BallObject *) frame->objects[i];
-
-					cv::circle(preview,
-						cv::Point(ball->visual.x, ball->visual.y),
-						ball->visual.radius,
-						cv::Scalar(0, 0, 255));
-				}
-				break;
-			}
-		}
-
-		snprintf(fpsString, 16, "FPS: %.f", vp.fps);
-		cv::putText(preview,
-			fpsString, fpsPosition,
-			cv::FONT_HERSHEY_SIMPLEX, 0.4,
-			cv::Scalar(0, 0, 255));
-
-		cv::imshow("Frame", preview);
-		cv::waitKey(1);
-
-		DebugLink::instance().frame(frame);
-
-		delete frame;
 	}
 
 	DebugLink::instance().msg(DebugLink::LEVEL_INFO, "main: Shutting down");
 	DebugLink::instance().close();
 
-	cv::destroyAllWindows();
 	capture.release();
 
 	return 0;
